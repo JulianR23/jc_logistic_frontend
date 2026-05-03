b853e7d59718b0ba2c4ca39e695178acabff2ec5
Julián Clavijo <130173701+JulianR23@users.noreply.github.com>
2026-05-03 09:55:37 -0500

    feat(store): add app stores and shared services

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+export { authService } from "./auth.service";
+export { clientsService } from "./clients.service";
+export { productsService } from "./products.service";
+export { warehousesService } from "./warehouses.service";
+export { portsService } from "./ports.service";
+export { shipmentsService } from "./shipments.service";
+export type { CreateShipmentPayload } from "./shipments.service";
