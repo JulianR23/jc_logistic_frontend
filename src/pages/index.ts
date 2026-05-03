5e558396b3f676cc986a11998dd86e99a38a2985
Julián Clavijo <130173701+JulianR23@users.noreply.github.com>
2026-05-03 09:57:57 -0500

    feat(pages): add pages index

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export * from "./ClientPage";
+export * from "./HomePage";
+export * from "./LoginPage";
+export * from "./SeaportPage";
+export * from "./ProductsPage";
+export * from "./ShipmentPage";
+export * from "./WarehousePage";
